2c3a724b03016ebe3a6fdf875ecd9143efd734dc
Julio <julio@gmail.com>
2022-10-26 16:55:26 -0500

    todavia no se aoptimiza javascript 1/2

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let infoCake = {
+let infoCake = {   // --------> Se crea un objeto donde se va almacenar los detalles del pastel
     size: '',
     firtsBiscuiit: '',
     secondBiscuiit: '',
@@ -6,58 +6,31 @@ let infoCake = {
     flavor: ''
 }
 
-let preBiscuiit = '';
+cakeSize = document.getElementById('cakeSize');// contenedo de seleccion de pastel
+firtsBiscuiit = document.getElementById('firtsBiscuiit');
+let selection = 'indefinido';
+//Se realiza la seleccion de tamaño del pastel
 
-infoCake.size = 'big';
-
-big= () =>{
-    console.log('se ejecuto');
-    infoCake.size = 'big';
-    let ocultar = document.getElementById('cakeSize');
-    let mostrar = document.getElementById('firtsBiscuiit');
-    ocultar.classList.add('hide');
-    mostrar.classList.remove('hide')
+function size(selected){
+    infoCake.size = selected;
+    cakeSize.classList.toggle("hide");
+    firtsBiscuiit.classList.toggle("hide");
+    console.log(infoCake)
 }
 
-function preSelection(tipo){
-    document.getElementById('fresa').classList.remove('under')
-    document.getElementById('fresa1').classList.remove('under')
-    document.getElementById('fresa2').classList.remove('under')
-    document.getElementById('vainilla').classList.remove('under')
-    document.getElementById('vainilla1').classList.remove('under')
-    document.getElementById('vainilla2').classList.remove('under')
-    document.getElementById("chocolate").classList.remove('under')
-    document.getElementById("chocolate1").classList.remove('under')
-    document.getElementById("chocolate2").classList.remove('under')
-    document.getElementById(tipo).classList.add('under')
-    preBiscuiit = tipo;
-    console.log(preBiscuiit)
-}
-
-function selected(lugar){
-    infoCake[lugar] = preBiscuiit;
-    console.log(infoCake);  
-    switch (lugar) {
-        case 'firtsBiscuiit':
-            document.getElementById('firtsBiscuiit').classList.add('hide');    
-            document.getElementById('secondBiscuiit').classList.remove('hide');           
-          break;
-        case 'secondBiscuiit':
-            document.getElementById('secondBiscuiit').classList.add('hide');    
-            document.getElementById('thirdBiscuiit').classList.remove('hide'); 
-        break;
-        case 'thirdBiscuiit':
-            document.getElementById('thirdBiscuiit').classList.add('hide');    
-            document.getElementById('flavor').classList.remove('hide');
-        break;
-      }
+//El under line de los textos al seleccionarlos
+function preSelection(biscuiit){
+    document.getElementById(biscuiit).classList.toggle("under")
+    if (selection !== 'indefinido'){
+        document.getElementById(selection).classList.toggle("under")
+    }
+    selection = biscuiit;
 
 }
 
-function end(flavor){
-    infoCake.flavor = flavor;
-    document.getElementById("amountChocolate").innerHTML = infoCake.size;
-}
+
+
+
 
 
 
